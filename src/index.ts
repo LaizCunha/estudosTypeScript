e2fc39f426f7134f4c060eb4b081ae3fbffb24a2
Laiz <laizsc.89@gmail.com>
2023-12-21 11:31:46 -0300

    Estudo de funções

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,26 @@
+// Funções
 
-let produto: object = {
-    name: "Carlos",
-    cidade: "Belém",
-    idade: 40,
-};
+function addNumber(x: number, y: number ): number {
+    return x + y;
+}
 
-type ProdutoLoja = {
-    nome: string;
-    preco: number;
-    unidades: number;
-};
+function addToHello(name: string) : string {
+    return `Hello, ${name}!`;
+}
 
-let meuProduto: ProdutoLoja = {
-    nome: "Tênis",
-    preco: 89.99,
-    unidades: 5,
-};
+function callToPhone(phone: number | string) : number | string {
+    return phone;
+}
 
-// Array
+async function getDatabase(id: number): Promise<number | string> {
+    return "Felipe";
+    
+}
 
-let dados: string[] = ["carlos", "felipe", "ana"];
-let dados2: Array<string> = ["carlos", "felipe", "ana"];
+let soma: number = addNumber(4,7);
 
-// Array Multiplos tipos
-
-let infos: (string | number)[] = ["felipe", 30, "carlos", 40];
-
-// Tuplas
-
-let boleto: [string, number, number] = ["agua conta", 150.8, 47756430];
-
-//Datas
-
-let aniversario:Date = new Date("2022-12-01 05:00");
-console.log(aniversario.toDateString());
-
-
+console.log(soma);
+console.log(addToHello("Felipe"));
+console.log(callToPhone(11313311));
+console.log(callToPhone("11313311"));
 
