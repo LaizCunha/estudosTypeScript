c1075e50b98759e4256f5f7719df02458efea371
Laiz <laizsc.89@gmail.com>
2023-12-21 10:48:36 -0300

    Estudo de objects

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+
+let produto: object = {
+    name: "Carlos",
+    cidade: "Belém",
+    idade: 40,
+};
+
+type ProdutoLoja = {
+    nome: string;
+    preco: number;
+    unidades: number;
+};
+
+let meuProduto: ProdutoLoja = {
+    nome: "Tênis",
+    preco: 89.99,
+    unidades: 5,
+};
