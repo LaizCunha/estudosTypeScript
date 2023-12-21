0d7fa2050972f4e2ba51d3195ce7772d38d646e7
Laiz <laizsc.89@gmail.com>
2023-12-21 11:18:25 -0300

    Diferença entre Array multiplas e Tuplas e uso do Date

--- src/index.ts
+++ src/index.ts
@@ -21,3 +21,19 @@ let meuProduto: ProdutoLoja = {
 
 let dados: string[] = ["carlos", "felipe", "ana"];
 let dados2: Array<string> = ["carlos", "felipe", "ana"];
+
+// Array Multiplos tipos
+
+let infos: (string | number)[] = ["felipe", 30, "carlos", 40];
+
+// Tuplas
+
+let boleto: [string, number, number] = ["agua conta", 150.8, 47756430];
+
+//Datas
+
+let aniversario:Date = new Date("2022-12-01 05:00");
+console.log(aniversario.toDateString());
+
+
+
